ec1e7b8a5d4c5bd70fc7641ac9b0c76e79a3779b
AleksandraTremba <117365775+AleksandraTremba@users.noreply.github.com>
2022-12-14 12:37:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,46 @@ else if(localStorage.getItem("theme") == "dark"){
 else{
     localStorage.setItem("theme", "light");
 }
+
+document.getElementById("drop").addEventListener("click", function(e) {
+    document.getElementById("myDropdown").classList.toggle("show");
+  });
+  
+  
+  // Close the dropdown if the user clicks outside of it
+  window.addEventListener("click", function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      document.querySelectorAll(".dropdown-content.show")
+        .forEach(openDropdown => openDropdown.classList.remove('show'))
+    }
+  });
+
+  const modal = document.querySelector(".modal")
+  const openModal = document.querySelector(".open-button")
+  const closeModal = document.querySelector(".close-button")
+
+  openModal.addEventListener('click', () => {
+    modal.showModal();
+  })
+
+  closeModal.addEventListener('click', () => {
+    modal.close();
+  })
+  const modal2 = document.querySelector(".modal2")
+  const openModal2 = document.querySelector(".open-button2")
+  const closeModal2 = document.querySelector(".close-button2")
+
+  openModal2.addEventListener('click', () => {
+    modal2.showModal();
+  })
+
+  closeModal2.addEventListener('click', () => {
+    modal2.close();
+  })
+
+  function func() {
+    document.getElementById('accountDetails').value  = 'Notifications Off';
+}
+  function func2() {
+    document.getElementById('accountDetails').value  = 'Followed';
+  }
